cd1638cd140c87d9e327a48efb3e5291fe40817a
alexevans <alexevans1122@gmail.com>
2016-02-17 17:47:34 -0500

    Changed Activty, removed SkillLevels

--- DB_scripts/Create_SkillLevels.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE IF NOT EXISTS `mydb`.`SkillLevels` (
-  `id` INT NOT NULL,
-  `skill` VARCHAR(45) NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE INDEX `id_UNIQUE` (`id` ASC))
-ENGINE = InnoDB
-COMMENT = 'This is the table containing the codes for the skill levels,' /* comment truncated */ /* adding or removing a skill level should be as simple as updating this table.*/
