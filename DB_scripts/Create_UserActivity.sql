ff41d2032d7c831a06f1d5694943ad8dcbd491ca
alexevans <alexevans1122@gmail.com>
2016-03-04 14:48:02 -0500

    DB latitude and longitude update

--- DB_scripts/Create_UserActivity.sql
+++ DB_scripts/Create_UserActivity.sql
@@ -1,6 +1,9 @@
-CREATE TABLE `useractivity` (
-  `userid` int(11) NOT NULL,
-  `activityid` int(11) NOT NULL,
-  `private_application` tinyint(1) DEFAULT NULL,
-  PRIMARY KEY (`userid`,`activityid`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='This is a Association entity, since Activity - User is a many to many relationship, use this to find activities and users matching.\nTo find activities for a user, do something like: SELECT FROM UserActivity WHERE USER.id = X\nTo find users on a specific activity, do something like: SELECT FROM UserActivity WHERE Activity.id = X';
+CREATE TABLE IF NOT EXISTS `mydb`.`useractivity` (
+  `userid` INT(11) NOT NULL,
+  `activityid` INT(11) NOT NULL,
+  PRIMARY KEY (`userid`, `activityid`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8
+COMMENT = ''/*This is a Association entity, since Activity - User is a many to many relationship, use this to find activities and users matching.
+To find activities for a user, do something like: SELECT FROM UserActivity WHERE USER.id = X
+To find users on a specific activity, do something like: SELECT FROM UserActivity WHERE Activity.id = X*/
