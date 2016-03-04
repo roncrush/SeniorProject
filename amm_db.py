3d2cf795a470d51102b41785ea605d373a17953d
Syed Hassaan Ali <syedali069@gmail.com>
2016-03-04 18:05:55 -0500

    Change connector _mysql to MySQLdb, Return  data as dicts

--- amm_db.py
+++ amm_db.py
@@ -1,21 +1,10 @@
-import _mysql
+import MySQLdb as mysql
 
 
 class AmmDB(object):
     def __init__(self):
-        self.conn = _mysql.connect(host='127.0.0.1', port=3306, user='root', passwd='admin', db='mydb')
-
-    @staticmethod
-    def pack(data):
-        rows = []
-        row = data.fetch_row()
-
-        while len(row) > 0:
-            for record in row:
-                rows.append(record)
-            row = data.fetch_row()
-
-        return rows
+        self.conn = mysql.connect(host='127.0.0.1', port=3306, user='root', passwd='admin', db='mydb')
+        self.cursor = self.conn.cursor(mysql.cursors.DictCursor)
 
     @staticmethod
     def get_where_stmnt(where_query, col_name, value, operator, compare=''):
@@ -34,11 +23,10 @@ class AmmDB(object):
         if self.conn.closed:
             self.__init__()
 
-        self.conn.query("SELECT email FROM user WHERE email = '" + email + "'")
-        data = self.conn.store_result()
-        row = data.fetch_row()
+        self.cursor.execute("SELECT email FROM user WHERE email = '" + email + "'")
+        data = self.cursor.fetchall()
 
-        if len(row) > 0:
+        if len(data) > 0:
             return True
         else:
             return False
@@ -47,11 +35,10 @@ class AmmDB(object):
         if self.conn.closed:
             self.__init__()
 
-        self.conn.query("SELECT uname FROM user WHERE uname = '" + uname + "'")
-        data = self.conn.store_result()
-        row = data.fetch_row()
+        self.cursor.execute("SELECT uname FROM user WHERE uname = '" + uname + "'")
+        data = self.cursor.fetchall()
 
-        if len(row) > 0:
+        if len(data) > 0:
             return True
         else:
             return False
@@ -65,18 +52,20 @@ class AmmDB(object):
         elif self.check_email_exist(email):
             return 'Email exists'
 
-        self.conn.query("INSERT INTO user (uname, email, passwd, phone, fn, ln, admin) VALUES ('" +
-                        uname + "' , '" + email + "' , '" + passwd + "' , " + phone + " , '" + fn + "' , '" +
-                        ln + "' , " + admin + ")")
+        self.cursor.execute("INSERT INTO user (uname, email, passwd, phone, fn, ln, admin) "
+                            "VALUES (%s, %s, %s, %s, %s, %s, %s)",
+                            (uname, email, passwd, phone, fn, ln, admin))
+        self.conn.commit()
 
     def add_activity(self, name, skill, datetime, duration, numplayers, private, available, category, leader):
         if self.conn.closed:
             self.__init__()
 
-        self.conn.query("INSERT INTO activity " +
-                        "(name, skill, datetime, duration, numplayers, private, available, category, leader) " +
-                        "VALUES ('" + name + "' , " + skill + " , '" + datetime + "' , " + duration + " , " +
-                        numplayers + " , " + private + " , " + available + " ," + category + " , " + leader + ")")
+        self.cursor.execute("INSERT INTO activity " +
+                            "(name, skill, datetime, duration, numplayers, private, available, category, leader) " +
+                            "VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)",
+                            (name, skill, datetime, duration, numplayers, private, available, category, leader, ))
+        self.conn.commit()
 
     def get_activity_type(self, activity_type_id='', name='', operator='AND'):
         if self.conn.closed:
@@ -96,10 +85,10 @@ class AmmDB(object):
                 else:
                     where_query += self.get_where_stmnt(where_query, param, str(value), operator)
 
-        self.conn.query("SELECT * FROM activitytype " + where_query)
-        data = self.conn.store_result()
+        self.cursor.execute("SELECT * FROM activitytype " + where_query)
+        data = self.cursor.fetchall()
 
-        return self.pack(data)
+        return data
 
     def get_activity(self, activity_id='', name='', skill='', duration='', numplayers='', available='', category='',
                      leader='', operator='AND'):
@@ -126,10 +115,10 @@ class AmmDB(object):
                 else:
                     where_query += self.get_where_stmnt(where_query, param, str(value), operator)
 
-        self.conn.query("SELECT * FROM activity " + where_query)
-        data = self.conn.store_result()
+        self.cursor.execute("SELECT * FROM activity " + where_query)
+        data = self.cursor.fetchall()
 
-        return self.pack(data)
+        return data
 
     def get_user(self, user_id='', uname='', email='', phone='', fn='', ln='', operator='AND'):
         if self.conn.closed:
@@ -138,13 +127,13 @@ class AmmDB(object):
         where_query = ""
 
         params = {
-                'id': user_id,
-                'uname': uname,
-                'email': email,
-                'phone': phone,
-                'fn': fn,
-                'ln': ln,
-            }
+            'id': user_id,
+            'uname': uname,
+            'email': email,
+            'phone': phone,
+            'fn': fn,
+            'ln': ln,
+        }
 
         for param, value in params.items():
             if value != '':
@@ -153,10 +142,10 @@ class AmmDB(object):
                 else:
                     where_query += self.get_where_stmnt(where_query, param, str(value), operator)
 
-        self.conn.query("SELECT * FROM user " + where_query)
-        data = self.conn.store_result()
+        self.cursor.execute("SELECT * FROM user " + where_query)
+        data = self.cursor.fetchall()
 
-        return self.pack(data)
+        return data
 
     def get_user_activity(self, user_id='', activity_id='', private_app='', operator='AND'):
         if self.conn.closed:
@@ -174,7 +163,7 @@ class AmmDB(object):
             if value != '':
                 where_query += self.get_where_stmnt(where_query, param, str(value), operator)
 
-        self.conn.query("SELECT * FROM useractivity " + where_query)
-        data = self.conn.store_result()
+        self.cursor.execute("SELECT * FROM useractivity " + where_query)
+        data = self.cursor.fetchall()
 
-        return self.pack(data)
+        return data
