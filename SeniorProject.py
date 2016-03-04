eb5b261a9c5a0bf6a7ad69c713142417a1f424db
Syed Hassaan Ali <syedali069@gmail.com>
2016-03-04 01:50:48 -0500

    Restructure into Flask app

---
+++ SeniorProject.py
@@ -0,0 +1,28 @@
+from flask import Flask, render_template
+import utilities
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def main_page():
+    return render_template('MainPage.html')
+
+
+@app.route('/home')
+def home():
+    return render_template('home.html')
+
+
+@app.route('/profile')
+def home():
+    return render_template('profile.html')
+
+
+@app.route('/create_event')
+def create_event():
+    return render_template('create_event.html', key=utilities.get_maps_key())
+
+
+if __name__ == '__main__':
+    app.run()
