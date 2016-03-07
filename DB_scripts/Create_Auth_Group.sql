b0ac93e54ace1132ca8cf5c69b3c0f0779e0b26f
alexevans <alexevans1122@gmail.com>
2016-03-07 16:09:07 -0500

    Changed password field and removed autogenerated scripts

--- DB_scripts/Create_Auth_Group.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE `auth_group` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(80) NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `name` (`name`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
