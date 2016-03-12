a7ac5834af2a97928229308437b9ab2abaef7434
echesnu2 <chesnut88@hotmail.com>
2016-03-12 18:06:06 -0500

    Added python package for Unit tests and python file for ammDB Unit tests

---
+++ Unittests/test_ammDB.py
@@ -0,0 +1,38 @@
+from unittest import TestCase
+from amm_db import AmmDB
+
+
+class TestAmmDB(TestCase):
+    def test_conn_check(self):
+        db = AmmDB
+        self.fail()
+
+    def test_get_where_stmnt(self):
+        self.fail()
+
+    def test_check_email_exist(self):
+        self.fail()
+
+    def test_check_uname_exist(self):
+        self.fail()
+
+    def test_add_user(self):
+        self.fail()
+
+    def test_add_activity(self):
+        self.fail()
+
+    def test_get_activity_type(self):
+        self.fail()
+
+    def test_get_activity(self):
+        self.fail()
+
+    def test_edit_user(self):
+        self.fail()
+
+    def test_get_user(self):
+        self.fail()
+
+    def test_get_user_activity(self):
+        self.fail()
