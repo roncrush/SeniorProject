4d460213e24fd93d6e1a7874676df0247a1980c2
echesnu2 <chesnut88@hotmail.com>
2016-03-14 21:28:04 -0400

    Worked on Unit tests for several methods

--- Unittests/test_ammDB.py
+++ Unittests/test_ammDB.py
@@ -39,96 +39,102 @@ class TestAmmDB(unittest.TestCase):
         self.assertTrue(db.check_uname_exist("test"), True)
 
     def test_get_user(self):
-        #first test output
-        #Positive:
-        #store observed get_user out
-        #store expected get_user out
-        #assertequal observed is expected
-        #Negative:
-        #store observed get_user out for bad params
-        #store expected get_user out for bad params
-        #assertequal observed is expected
-        self.fail()
+        # Positive
+        db = AmmDB()
+        passwd = b'$2b$12$Z2OaKVc39OH6duIxaKFnkefKztlq7oPiYpzdNHfSwQDvBRfFjVCJ6'
+        observed = db.get_user(9, 'test', 'test@test.com', '1234567890', 'test', 'test', 'AND', False)
+        expected = ({'suspension': None, 'admin': 0, 'email': 'test@test.com', 'uname': 'test', 'passwd': passwd, 'id': 9, 'phone': '1234567890', 'ln': 'test', 'fn': 'test'},)
+        self.assertEqual(observed, expected)
+
+        # Negative
+        observed = db.get_user('-1', 'bad', 'bad@bad.com', '-11111111', 'bad', 'bad', 'AND', False)
+        expected = ()
+        self.assertEqual(observed, expected)
+
 
     def test_add_user(self):
-        #Positive
-        #Add user
-        #get user
-        #store observed get_user out
-        #store expected get_user out
-        #assertequal observed is expected
-        #Negative
-        #Add user with bad info
-        #get user
-        #store observed get_user out
-        #store expected get_user out
-        #assertnotequal observed is expected
-        self.fail()
+        # Positive
+        db = AmmDB()
+        db.add_user('test', 'test@test.com', 'test', 'test', 'test', 0, '1234567890')
+        passwd = b'$2b$12$Z2OaKVc39OH6duIxaKFnkefKztlq7oPiYpzdNHfSwQDvBRfFjVCJ6'
+        observed = db.get_user(9, 'test', 'test@test.com', '1234567890', 'test', 'test', 'AND', False)
+        expected = ({'suspension': None, 'admin': 0, 'email': 'test@test.com', 'uname': 'test', 'passwd': passwd, 'id': 9, 'phone': '1234567890', 'ln': 'test', 'fn': 'test'},)
+        self.assertEqual(observed, expected)
+
+        # Negative
+        # Add user with bad info
+        # get user
+        # store observed get_user out
+        # store expected get_user out
+        # assertnotequal observed is expected
 
     def test_get_activity(self):
-        #first test output
-        #Positive:
-        #store observed get_act out
-        #store expected get_act out
-        #assertequal observed is expected
-        #Negative:
-        #store observed get_act out for bad params
-        #store expected get_act out for bad params
-        #assertequal observed is expected
-        self.fail()
+        # Positive
+        db = AmmDB()
+        # observed = db.get_activity(1, 'testName', 4, 1, 3, 1, 25, 9, 'AND')
+        # expected = ({'id': 1, 'name': 'testName', 'numplayers': 3, 'available': 1, 'skill': 4, 'category': 25, 'longitude': Decimal('0.0000'), 'datetime': datetime.datetime(2017, 4, 13, 18, 36, 49), 'private': 0, 'latitude': Decimal('0.0000'), 'leader': 9, 'duration': 1})
+        # self.assertEqual(observed, expected)
+
+        # Negative
+        observed = db.get_activity(0, 'badName', 4, 1, 3, 1, 25, 9, 'AND')
+        expected = ()
+        self.assertEqual(observed, expected)
 
     def test_add_activity(self):
-        #Positive
-        #Add user
-        #get user
-        #store observed get_act out
-        #store expected get_act out
-        #assertequal observed is expected
-        #Negative
-        #Add user with bad info
-        #get user
-        #store observed get_act out
-        #store expected get_act out
-        #assertnotequal observed is expected
+        # Positive
+        # Add user
+        # get user
+        # store observed get_act out
+        # store expected get_act out
+        # assertequal observed is expected
+        # Negative
+        # Add user with bad info
+        # get user
+        # store observed get_act out
+        # store expected get_act out
+        # assertnotequal observed is expected
         self.fail()
 
     def test_get_activity_type(self):
-        # db = AmmDB()
-        # observed = db.get_activity_type('25', 'testName', 'AND')
-        # expected = "25 Arts and Crafts"
-        # self.assertEqual(observed, expected)
-        self.fail()
+        # positive
+        db = AmmDB()
+        observed = db.get_activity_type('25', 'Arts and Crafts', 'AND')
+        expected = ({'id': 25, 'name': 'Arts and Crafts'},)
+        self.assertEqual(observed, expected)
+
+        # negative
+        observed = db.get_activity_type('0', 'Invalid', 'AND')
+        expected = ()
+        self.assertEqual(observed, expected)
 
     def test_edit_user(self):
-        #Positive
-        #Edit user
-        #get user
-        #store observed get_user out
-        #store expected get_user out
-        #assertequal observed is expected
-        #Negative
-        #Add user with bad info
-        #get user
-        #store observed get_user out
-        #store expected get_user out
-        #assertnotequal observed is expected
-        self.fail()
+        # Positive
+        # db = AmmDB()
+        # db.edit_user()
+        # observed = db.get_user()
+        # expected = ()
+        # self.assertEqual(observed, expected)
 
+        # Negative
+        # Add user with bad info
+        # get user
+        # store observed get_user out
+        # store expected get_user out
+        # assertnotequal observed is expected
+        self.fail()
 
     def test_get_user_activity(self):
-        #Positive
-        #Edit usr_act
-        #get act
-        #store observed get_usract out
-        #store expected get_usract out
-        #assertequal observed is expected
-        #Negative
-        #Edit useract with bad info
-        #get useract
-        #store observed get_useract out
-        #store expected get_useract out
-        #assertnotequal observed is expected
-        self.fail()
+        # Positive
+        db = AmmDB()
+        #looks like we are missing private_application in the DB
+        #observed = db.get_user_activity(9, 25, '', 'AND')
+        #expected = ()
+        #self.assertEqual(observed, expected)
+
+        # Negative
+        observed = db.get_user_activity(-1, 25, '', 'AND')
+        expected = ()
+        self.assertEqual(observed, expected)
 
 if __name__ == '__main__':
     unittest.main()
