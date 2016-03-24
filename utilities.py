86a66b39efec2ff60391d4615cd876b423802648
Syed Hassaan Ali <syedali069@gmail.com>
2016-03-24 19:00:18 -0400

    Make get_maps_key generic get_key

--- utilities.py
+++ utilities.py
@@ -1,6 +1,7 @@
 import time
 from time import mktime
 from datetime import datetime
+import requests as r
 
 
 def combine_datetime(date, time_input):
@@ -10,7 +11,22 @@ def combine_datetime(date, time_input):
     return final_datetime
 
 
-def get_maps_key():
+def get_key(key_name):
     with open('key.txt', 'r') as key_file:
-        key = key_file.read()
-    return key
+        for line in key_file:
+            if key_name in line:
+                return line.split(':')[1].strip()
+    return None
+
+
+def send_email(recipient, subject, message, frm='ActivityMatchMaker@example.com'):
+    key = get_key('mailgun_key')
+    sandbox = get_key('mailgun_sandbox')
+    request_url = 'https://api.mailgun.net/v3/{0}/messages'.format(sandbox)
+
+    r.post(request_url, auth=('api', key), data={
+        'from': frm,
+        'to': recipient,
+        'subject': subject,
+        'text': message
+    })
